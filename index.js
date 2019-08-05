8daa25b652cbc15c9202892970faa4d6055fb232
Michelle Lam <shelly2.lam@gmail.com>
2019-08-05 18:05:15 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,31 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+
+const superbowlWin = (records) => {
+ let event = records.find(function(record) {
+  return record.result === "W"
+ });
+  if (event) {
+    return event.year
+  }
+    else {
+      return undefined
+    }
+}
+
+// const newSuperbowlWin = (records) => {
+//   return records.find(function(record) {
+//     if (record.result === "W") {
+//     return record.year}
+//   })
+}
